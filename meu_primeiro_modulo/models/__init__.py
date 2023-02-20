911f1ab6bcc3e269a3e8e0e72bf1a8da6daa3c01
Daniel Nery <danielpontesnery@gmail.com>
2023-02-20 12:16:17 -0300

    add: Primeiro Módulo Odoo.

---
+++ meu_primeiro_modulo/models/__init__.py
@@ -0,0 +1,1 @@
+from . import meu_primeiro_modelo
---
+++ meu_primeiro_modulo/models/meu_primeiro_modelo.py
@@ -0,0 +1,5 @@
+from odoo import models
+
+class MeuPrimeiroModulo(models.Model):
+    _name = 'meu.primeiro.modelo'
+    _description = "meu primeiro modelo criado no Odoo"
