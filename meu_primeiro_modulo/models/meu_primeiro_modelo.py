7db66b8ffbb8af89cb34dff82ff7c563e46d4ef2
LuanPetruitis <luanpetruitis@hotmail.com>
2023-03-01 12:49:48 -0300

    Adicionando campo Inteiro.

--- meu_primeiro_modulo/models/meu_primeiro_modelo.py
+++ meu_primeiro_modulo/models/meu_primeiro_modelo.py
@@ -1,5 +1,11 @@
-from odoo import models
+from odoo import models, fields
 
 class MeuPrimeiroModulo(models.Model):
     _name = 'meu.primeiro.modelo'
     _description = "meu primeiro modelo criado no Odoo"
+    
+    meu_primeiro_campo_inteiro = fields.Integer(
+        string="Número Inteiro", 
+        default=0,
+        help="Exemplo"
+    )
