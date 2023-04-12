31832434efa60acb07bbb45018a62d8d4542246b
LuanPetruitis <luanpetruitis@hotmail.com>
2023-04-12 13:34:55 -0300

    Novos campos

--- meu_primeiro_modulo/models/meu_primeiro_modelo.py
+++ meu_primeiro_modulo/models/meu_primeiro_modelo.py
@@ -4,8 +4,41 @@ class MeuPrimeiroModulo(models.Model):
     _name = 'meu.primeiro.modelo'
     _description = "meu primeiro modelo criado no Odoo"
     
+    # default=True - Valor padrão do campo
+    # readonly=True - Se é um campo para somente leitura
+    # required=True - Define se o campo é obrigatório
+    # store=True - Define se o campo é obrigatóriamente salvo no banco de dados, os campos por padrão
+    #       São store True, eles só se tornam store false quando tem o comnpute
+    # compute="função_para_executar_tarefa" - Quando você deseja que o campo seja computado 
+    # help="string" - Para ajudar o usuário saber para que serve o campo
+    
+    
     meu_primeiro_campo_inteiro = fields.Integer(
         string="Número Inteiro", 
         default=0,
         help="Exemplo"
     )
+
+    primeiro_campo_boolean = fields.Boolean(
+        string="Madeira",
+        help="Campo para definir se a matéria prima do produto é de madeira ou não"
+    )
+    
+    # Exemplo => 1.000.000.000,00
+    campo_float = fields.Float(
+        string="Campo Float",
+        digits=(10, 2)
+    )
+    
+    name = fields.Char(
+        string="Nome"
+    )
+    
+    descricao = fields.Text(
+        string="Descrição"
+    )
+    
+    tipo = fields.Selection([
+        ('ativo', 'Ativo'), 
+        ('inativo', 'Inativo'),
+    ], string="Tipo")
