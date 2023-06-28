9fb34ea4eb53ab7b0f827cb176cd263dae47afee
Daniel Nery <danielpontesnery@gmail.com>
2023-06-28 14:01:38 -0300

    feat: Primeira view kanban.

--- meu_primeiro_modulo/__manifest__.py
+++ meu_primeiro_modulo/__manifest__.py
@@ -3,6 +3,9 @@
     "description": "meu primeiro módulo",
     "summary": "meu primeiro módulo que faz nada",
     "author": "Fulano de tal",
+    "depends": [
+        "sale"
+    ],
     "data": [
         "security/ir.model.access.csv",
         "views/meu_primeiro_modelo_views.xml",
